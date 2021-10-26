33b2a48a0ee764243ca1db05eca439c989d95a44
Beckett Brees <bbreespt@gmail.com>
2021-10-25 21:46:09 -0400

    first py file

---
+++ restaurant_pick.py
@@ -0,0 +1,4 @@
+location = input('Where do you want to eat tonight?')
+flavor = input('What kind of food are you interested in tonight?')
+
+print('I am looking for a restaurant in', location, 'that serves', flavor)
