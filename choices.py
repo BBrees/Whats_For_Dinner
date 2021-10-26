2454eef97d4a61eb7044ee7e16aa73e2c085f1b6
Beckett Brees <bbreespt@gmail.com>
2021-10-25 22:48:20 -0400

    randomization

---
+++ choices.py
@@ -0,0 +1,3 @@
+ShelbyPark = ["Toasty's Tavern","Square Cut Pizza", "Red Top Hot Dogs"]
+GermanTown = ["Four Pegs", "Six Forks", "The Post"]
+OldLouisville = ["Pizza Donsi", "Old Louisville Brewing", "Mag Bar"]
--- restaurant_pick.py
+++ restaurant_pick.py
@@ -1,4 +1,17 @@
-location = input('Where do you want to eat tonight?')
-flavor = input('What kind of food are you interested in tonight?')
+import requests
+import random
+import choices
 
-print('I am looking for a restaurant in', location, 'that serves', flavor)
+#api_key = "wWo4R0IdYfTs35U8WcJvA1azDCVbGPVEx3POlFUUbTHNwkjQAMiA7kRUusiCOR4GJoP9WIYM1B8n_YTA6BFHcKepDcmPGvVUXPGznLZjZ9iW82Oubgss5tnFFmJ3YXYx"
+
+#url = "https://api.yelp.com/v3/businesses/search"
+
+
+
+location = input('Where do you want to eat tonight?')
+
+#flavor = input('What kind of food are you interested in tonight?')
+
+print("Try:")
+
+print(random.choice(choices.ShelbyPark))
