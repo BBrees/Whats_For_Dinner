4f07c6d296fe7cb843228cb3ff479423c1c00f2d
Beckett Brees <bbreespt@gmail.com>
2021-11-04 08:12:58 -0400

    api connected

--- choices.py
+++
@@ -1,3 +0,0 @@
-ShelbyPark = ["Toasty's Tavern","Square Cut Pizza", "Red Top Hot Dogs"]
-GermanTown = ["Four Pegs", "Six Forks", "The Post"]
-OldLouisville = ["Pizza Donsi", "Old Louisville Brewing", "Mag Bar"]
--- yelp_data.py
+++ yelp_data.py
@@ -1,7 +1,39 @@
 import requests
 import json
+import random
 
-api_key = "Secret Token"
+#connect to external api
+api_key = "SECRET_TOKEN"
 headers = {'Authorization': 'Bearer %s' % api_key}
 
 url = "https://api.yelp.com/v3/businesses/search"
+
+#type of food and location in louisville 
+
+def Restaurants():
+
+#Create a dictionary or list, populate it with several values, retrieve at least one to use in your program
+
+    params = {}
+    
+    #input function call twice in order to build dictionary
+    params['term'] = input("What kind of food would you like for dinner?\n")
+    params['location'] = input("Where do you want to eat?\n")
+
+#Utilizing dictionary to filter requests to yelp api
+    req = requests.get(url, params=params, headers=headers)
+    parsed = json.loads(req.text)
+    businesses = parsed["businesses"]
+    
+    dinner = random.choice(businesses)
+
+    #return for Restaurant function
+
+    return dinner
+
+Whats_For_Dinner = Restaurants()
+
+#print function utilized to display return of Restaurant function
+
+print (Whats_For_Dinner)
+
